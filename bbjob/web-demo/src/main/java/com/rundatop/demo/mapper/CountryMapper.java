5616bbf45433702f3cc9eb32ca2839c558778ce8
zhujiang0007 <zhujiang0007@foxmail.com>
2017-02-06 08:30:28 +0800

    基础上传

---
+++ bbjob/web-demo/src/main/java/com/rundatop/demo/mapper/CountryMapper.java
@@ -0,0 +1,7 @@
+package com.rundatop.demo.mapper;
+
+import com.rundatop.demo.model.Country;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CountryMapper extends Mapper<Country> {
+}
---
+++ bbjob/web-demo/src/main/java/com/rundatop/demo/mapper/UserInfoMapper.java
@@ -0,0 +1,7 @@
+package com.rundatop.demo.mapper;
+
+import com.rundatop.demo.model.UserInfo;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserInfoMapper extends Mapper<UserInfo> {
+}
