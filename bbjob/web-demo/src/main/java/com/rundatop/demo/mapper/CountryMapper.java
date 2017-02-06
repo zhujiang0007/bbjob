373e0ed9ef5cb07c4052828f792900e5624b4fc3
zhujiang0007 <zhujiang0007@foxmail.com>
2017-02-06 23:03:51 +0800

    删除废弃项目

--- bbjob/web-demo/src/main/java/com/rundatop/demo/mapper/CountryMapper.java
+++
@@ -1,7 +0,0 @@
-package com.rundatop.demo.mapper;
-
-import com.rundatop.demo.model.Country;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface CountryMapper extends Mapper<Country> {
-}
--- bbjob/web-demo/src/main/java/com/rundatop/demo/mapper/UserInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.rundatop.demo.mapper;
-
-import com.rundatop.demo.model.UserInfo;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UserInfoMapper extends Mapper<UserInfo> {
-}
