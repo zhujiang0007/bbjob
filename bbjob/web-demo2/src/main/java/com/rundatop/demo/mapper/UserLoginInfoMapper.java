cdc833898c610a1fcea592c091c4492a804ab405
zhujiang0007 <zhujiang0007@foxmail.com>
2017-02-07 08:58:22 +0800

    添加抽象service与示例mapper

---
+++ bbjob/web-demo2/src/main/java/com/rundatop/demo/mapper/UserLoginInfoMapper.java
@@ -0,0 +1,7 @@
+package com.rundatop.demo.mapper;
+
+import com.rundatop.demo.model.UserLoginInfo;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserLoginInfoMapper extends Mapper<UserLoginInfo> {
+}
