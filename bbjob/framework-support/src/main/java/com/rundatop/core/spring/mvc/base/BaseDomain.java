4055e954e20a3170589a64d3b0ceb1383d3815a6
zhujiang0007 <zhujiang0007@foxmail.com>
2017-02-08 11:05:56 +0800

    删除冗余代码

--- bbjob/framework-support/src/main/java/com/rundatop/core/spring/mvc/base/BaseDomain.java
+++
@@ -1,9 +0,0 @@
-package com.rundatop.core.spring.mvc.base;
-
-import java.io.Serializable;
-
-public class BaseDomain implements Serializable{
-
-	private static final long serialVersionUID = -5467307169003728576L;
-
-}
